16145586e487daca7f8093942f6e8532195c726c
Abumuslim Abakarov <abakarovmuslim26@gmail.com>
2025-04-12 21:18:15 +0500

    feat: добавлены кастомные permissions для админов и владельцев

---
+++ library_backend/api/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework import permissions
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return request.user and request.user.is_authenticated and request.user.is_admin
+
+class IsOwnerOrAdminOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return (
+            request.user and
+            request.user.is_authenticated and
+            (obj.user == request.user or request.user.is_admin)
+        )
